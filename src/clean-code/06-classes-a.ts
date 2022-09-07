ceb5426de4947aa1fcb78cc0788717455488a2e6
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-09-06 21:01:40 -0500

    Classes - NO Single responsibility principle

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -1,5 +1,7 @@
 (() => {
 
+    // No aplicando el principio de responsabilidad única
+
     type Gender = 'M' | 'F';
 
     class Person {
@@ -8,11 +10,54 @@
             public name: string,
             public gender: Gender,
             public birthDate: Date,
-        ) {}
+        ) {
+        }
 
     }
 
-    const newPerson = new Person('Arian', 'M', new Date('2000-07-06'));
-    console.log({newPerson});
+    class User extends Person {
+        public lastAccess: Date;
+
+        constructor(
+            public email: string,
+            public role: string,
+            name: string,
+            gender: Gender,
+            birthDate: Date,
+        ) {
+            super(name, gender, birthDate);
+            this.lastAccess = new Date();
+        }
+
+        checkCredentials() {
+            return true
+        }
+    }
+
+    class UserSettings extends User {
+        constructor(
+            public workingDirectory: string,
+            public lastOpenFolder: string,
+            email: string,
+            role: string,
+            name: string,
+            gender: Gender,
+            birthDate: Date,
+        ) {
+            super(email, role, name, gender, birthDate);
+        }
+    }
+
+    const newUserSettings = new UserSettings(
+        'C:\\',
+        'C:\\',
+        'arian@gmail.com',
+        'admin',
+        'Arian',
+        'M',
+        new Date('2000-06-07'),
+    );
+
+    console.log({newUserSettings});
 
 })();
