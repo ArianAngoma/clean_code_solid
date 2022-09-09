558dd7554363f8d2a43645232e80c7b0aa8203e4
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-09-08 20:47:22 -0500

    SOLID - ISP(Segregation de Interzas)

--- src/solid/04-segregation.ts
+++ src/solid/04-segregation.ts
@@ -1,58 +1,46 @@
 interface Bird {
-    fly(): void;
-
     eat(): void;
+}
 
+interface FlyingBird {
+    fly(): number;
+}
+
+interface RunningBird {
     run(): void;
+}
 
+interface SwimmingBird {
     swim(): void;
 }
 
-class Tucan implements Bird {
+class Tucan implements Bird, FlyingBird {
 
     public fly() {
         console.log('I can fly');
+        return 10
     }
 
     public eat() {
         console.log('I can eat');
     }
 
-    public run() {
-        console.log('I can run');
-    }
-
-    public swim() {
-        throw new Error('I can not swim');
-    }
-
 }
 
-class Hummingbird implements Bird {
+class Hummingbird implements Bird, FlyingBird {
 
     public fly() {
         console.log('I can fly');
+        return 100;
     }
 
     public eat() {
         console.log('I can eat');
-    }
-
-    public run() {
-        console.log('I can run');
-    }
-
-    public swim() {
-        throw new Error('I can not swim');
     }
 
 }
 
-class Ostrich implements Bird {
-
-    public fly() {
-        throw new Error('I can not fly');
-    }
+class Ostrich implements Bird, RunningBird {
 
     public eat() {
         console.log('I can eat');
@@ -60,26 +48,14 @@ class Ostrich implements Bird {
 
     public run() {
         console.log('I can run');
-    }
-
-    public swim() {
-        throw new Error('I can not swim');
     }
 
 }
 
-class Penguin implements Bird {
-
-    public fly() {
-        throw new Error('I can not fly');
-    }
+class Penguin implements Bird, SwimmingBird {
 
     public eat() {
         console.log('I can eat');
-    }
-
-    public run() {
-        console.log('I can run');
     }
 
     public swim() {
